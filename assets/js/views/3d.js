cdc06bcec9611d7e382d582090784da8369206b6
sorrymeika <sorrymeika@163.com>
2014-08-21 16:01:20 +0800

    fix

--- assets/js/views/3d.js
+++ assets/js/views/3d.js
@@ -1,54 +1,120 @@
-﻿define('views/3d',['zepto','ui/sl','app','views/loading','util'],function (require,exports,module) {
+﻿define('views/3d',['zepto','ui/sl','app','views/loading','util'],function(require,exports,module) {
     var $=require('zepto'),
         sl=require('ui/sl'),
         app=require('app'),
         util=require('util'),
         Loading=require('views/loading');
 
+
+    var sumList={
+        n3: 1,
+        n4: 1,
+        n5: 2,
+        n6: 3,
+        n7: 4,
+        n8: 5,
+        n9: 7,
+        n10: 8,
+        n11: 9,
+        n12: 10,
+        n13: 10,
+        n14: 10,
+        n15: 10,
+        n16: 9,
+        n17: 8,
+        n18: 7,
+        n19: 5,
+        n20: 4,
+        n21: 3,
+        n22: 2,
+        n23: 1,
+        n24: 1
+    };
+
     module.exports=sl.Activity.extend({
         events: {
             'tap .J_Back': 'back',
             'tap .ballPool.red .bd em': 'selectRed',
             'tap .J_Buy': "buy",
+            'tap .J_Type': 'showType',
             'tap .J_Clear': 'clear',
             'tap .J_Random': 'random',
-            'tap .J_RandomRed': 'randomRed'
+            'tap .J_RandomRed': 'randomRed',
+            'tap .J_TypeCon li': function(e) {
+                var $target=$(e.currentTarget);
+
+                $target.addClass('curr').siblings('.curr').removeClass('curr');
+
+                if($target.index()==0) {
+                    $('.J_Random').show();
+                } else {
+                    $('.J_Random').hide();
+                }
+
+                this.$('.js_group_cont'+$target.index()).show().siblings('.js_group_cont').hide();
+                this.showType();
+            }
         },
 
-        buy: function () {
+        showType: function() {
+            this.$('.J_Type,.J_TypeCon').toggleClass('visible');
+            this.$('header').toggleClass('type_visible');
+        },
+
+        buy: function() {
             var that=this,
                 betData="",
                 flag=true,
                 type='01',
                 codes='',
-                codes02='';
+                codes02='',
+                typeIndex=that.$('.J_TypeCon li.curr').index();
 
-            that.$red.each(function () {
-                var $items=$(this).find('em.curr');
+            if(typeIndex==0) {
+                that.$red.each(function() {
+                    var $items=$(this).find('em.curr');
 
-                if($items.length==0) {
-                    flag=false;
-                } else if($items.length>1) {
-                    type='02';
+                    if($items.length==0) {
+                        flag=false;
+                    } else if($items.length>1) {
+                        type='02';
+                    }
+
+                    codes02+=util.pad($items.length,2);
+                    $items.each(function() {
+                        codes+=util.pad(this.innerHTML,2);
+                        codes02+=util.pad(this.innerHTML,2);
+                    });
+                });
+
+                if(type=='01') {
+                    betData="01|01|0001|"+codes;
+                } else {
+                    betData="01|02|0001|"+codes02;
                 }
 
-                codes02+=util.pad($items.length,2);
-                $items.each(function () {
-                    codes+=util.pad(this.innerHTML,2);
-                    codes02+=util.pad(this.innerHTML,2);
+            } else if(typeIndex==1) {
+                that.$('.js_group_cont'+typeIndex+' .redBallList').each(function(i) {
+                    var $items=$(this).find('em.curr');
+
+                    if($items.length==0) {
+                        flag=false;
+                    }
+
+                    codes+=util.pad($items.html(),2);
+
+                    if(i==0) {
+                        codes+=codes;
+                    }
                 });
-            });
+                betData="02|01|0001|"+codes;
+            } else {
+                flag=false;
+            }
 
             if(!flag) {
                 sl.tip("请至少选择一注");
                 return;
-            }
-
-            if(type=='01') {
-                betData="01|01|0001|"+codes;
-            } else {
-                betData="01|02|0001|"+codes02;
-
             }
 
             console.log(betData);
@@ -60,10 +126,10 @@
 
             that.to('/3dBuy.html');
         },
-        random: function () {
+        random: function() {
             this.randomRed();
         },
-        randomRed: function () {
+        randomRed: function() {
             var that=this;
 
             that.$red.find('em.curr').removeClass('curr');
@@ -73,7 +139,7 @@
             }
 
             var i=3;
-            that.redTimer=setInterval(function () {
+            that.redTimer=setInterval(function() {
 
                 var num=Math.round(Math.random()*8);
 
@@ -92,34 +158,46 @@
             },100);
 
         },
-        clear: function () {
+        clear: function() {
             this.$('.ballPool.red .bd em.curr').removeClass('curr');
         },
-        selectRed: function (e) {
+        selectRed: function(e) {
             var $target=$(e.currentTarget);
             $target.toggleClass('curr');
+
+            if($target.closest('.redBallList').hasClass('js_single')) {
+                $target.closest('p').siblings('p').find('em.curr').removeClass('curr');
+            }
         },
-        selectBlue: function (e) {
+        selectBlue: function(e) {
             $(e.currentTarget).toggleClass('curr');
         },
         template: 'views/3d.html',
-        onCreate: function () {
+        onCreate: function() {
             var that=this,
                 html="<li>";
+
+            that.$type=that.$('.J_TypeCon');
 
             for(var i=0;i<=9;i++) {
                 html+='<p><em data-red="'+i+'">'+i+'</em></p>';
                 if((i+1)%8==0) { html+="</li><li>"; }
             }
-            that.$('.J_RedList').append(html+"</li>");
+            that.$('.J_RedList,.J_Group3List,.J_Group6List').append(html+"</li>");
 
             html="<li>";
+
+            for(var i=3;i<=24;i++) {
+                html+='<p><em data-red="'+i+'">'+i+'</em></p>';
+                if((i-2)%8==0) { html+="</li><li>"; }
+            }
+            that.$('.J_SumList').append(html+"</li>");
 
             that.$red=that.$('.J_RedList');
 
             that.$el.loading('load',{
                 url: '/api/CPService/QueryGameXspar/?ct=json&gameid=10002&wagerissue=',
-                success: function (res) {
+                success: function(res) {
 
                     that.$('.js_curPhase').html(res.Data[0].WagerIssue);
                     var endTime=new Date(res.Data[0].DrawEndTime.replace(/T|\:/,'-').split('-')),
@@ -132,7 +210,7 @@
                     } else {
                         that.$('.js_leftTime').html("投注剩余"+that.parseTime(leftTime));
 
-                        that.interval=setInterval(function () {
+                        that.interval=setInterval(function() {
                             leftTime--;
                             if(leftTime<=0) {
                                 that.isOver=true;
@@ -145,10 +223,10 @@
                         },1000);
                     }
                 },
-                error: function (xhr) {
+                error: function(xhr) {
                     if(xhr.status==500||xhr.status==401) {
                         this.msg('还未登录...');
-                        setTimeout(function () {
+                        setTimeout(function() {
                             that.to('/login.html');
                         },1000);
                     } else
@@ -156,7 +234,7 @@
                 }
             });
         },
-        parseTime: function (s) {
+        parseTime: function(s) {
             var h=Math.floor(s/(60*60));
             s=s-h*60*60;
             m=Math.floor(s/60);
@@ -164,12 +242,12 @@
 
             return h+"时"+m+"分"+s+"秒";
         },
-        onStart: function () {
+        onStart: function() {
         },
-        onResume: function () {
+        onResume: function() {
             this.clear();
         },
-        onDestory: function () {
+        onDestory: function() {
             this.interval&&clearInterval(this.interval);
         }
     });
